56bba09aa902c4d5dc4ba929ad47f32c3bd36f3e
iy88 <1845767798@qq.com>
2020-07-27 21:48:03 +0800

    update

--- src/routes/callback.js
+++ src/routes/callback.js
@@ -1,22 +1,30 @@
 const lib = require('../lib/githubOAuth')(require('../config'));
-async function handle(req, res) { //啥，async不懂？百度去...
+/**
+ * 核心逻辑: 
+ *   1. 获取code，code为用户github授权登录后传递到这里的query中
+ *   2. 获取token，将获取到的code请求github验证api，拿到token
+ *   3. 获取user，将获取到的token（access token）在请求用户数据时传递，拿到用户公开数据
+ * @param {*} req 
+ * @param {*} res 
+ */
+async function handle(req, res) { // 啥，async还是不懂？百度去...
   if (req.query.code) {
     try {
-      let { data } = await lib.getToken(req.query.code);   //啥，await不懂？百度去...
-      if (!data.error) { //判断code是否过期或已使用
-        let token = data['access_token']; //access token 有了
+      let { data } = await lib.getToken(req.query.code);   // 啥，await还是不懂？百度去...
+      if (!data.error) { // 判断code是否过期或已使用
+        let token = data['access_token']; // access token 有了
         let r = await lib.getUserInfo(token); // 防止和res或data冲突，此处的r意为response
-        //这里之所以不需要判断是否错误，是因为token不会有问题，而且立即发送请求没有过期问题
-        req.session.user = r.data.login; //将用户存进session
-        req.session.email = r.data.email; //将用户邮箱存进session
-        req.session.avatar = r.data.avatar_url; //将用户头像存入session
+        // 这里之所以不需要判断是否错误，是因为token不会有问题，而且立即发送请求没有过期问题
+        req.session.user = r.data.login; // 将用户存进session
+        req.session.email = r.data.email; // 将用户邮箱存进session
+        req.session.avatar = r.data.avatar_url; // 将用户头像存入session
         req.session.save(() => {
           res.redirect(302, '/login'); //成功获取
         })
       } else {
         res.redirect(302, 'login'); // code过期或已使用
       }
-    } catch (e) { //异常捕获（由于某Firewall）
+    } catch (e) { //异常捕获（由于某firewall）
       res.redirect(302, '/login');
     }
   } else {
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,7 @@
 const URI = require('../lib/githubOAuth')(require('../config')).redirectUrl;
 module.exports = (req,res)=>{
-  if(req.session.user){   //已登录，无需再次oauth
+  if(req.session.user){   //已登录，无需再次认证
+    // 注 req.session 和在 callback 中存入的 session 是同一个
     res.send(`<h3>username: ${req.session.user}</h3><h3>email: ${req.session.email}</h3><img src="${req.session.avatar}">`);
   }else{
     res.redirect(302,URI);
