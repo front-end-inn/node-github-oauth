79fa0e7263bc74cc4be25a6096af16dd08163b3e
iy88 <1845767798@qq.com>
2020-07-27 11:46:45 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const app = require('./app');
-http.createServer(app).listen(3000,()=>{
-  console.log('server start at http://localhost:3000')
+http.createServer(app).listen(3000,'0.0.0.0',()=>{ // 3000是端口 0.0.0.0是主机名,0.0.0.0意为任意主机名访问
+  console.log('server start at http://localhost:3000'); 
 });
